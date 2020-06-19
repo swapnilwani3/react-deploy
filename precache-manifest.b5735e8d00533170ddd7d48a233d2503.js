099f7442d643d78f25cb67c7f5a09baa066fae0e
swapnilwani3 <waniswapnil3@gmail.com>
2020-06-19 14:55:30 +0530

    Updates

---
+++ precache-manifest.b5735e8d00533170ddd7d48a233d2503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9558a36a689f76ee9331bcfddd7dbe2",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "fff23a8a935792d219ea",
+    "url": "/react-deploy/static/css/main.80e97272.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff23a8a935792d219ea",
+    "url": "/react-deploy/static/js/main.b6189540.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "a6c91ae25bbb844c5cf003c449b71004",
+    "url": "/react-deploy/static/media/nature.a6c91ae2.jpg"
+  }
+]);
